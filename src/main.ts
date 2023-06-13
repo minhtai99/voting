c646a4256c0730687e83d46d4011f9c1e6f533fa
TriLe <tri.le@siliconstack.com.au>
2023-06-13 10:21:00 +0700

    remove redundant code

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     credentials: true,
